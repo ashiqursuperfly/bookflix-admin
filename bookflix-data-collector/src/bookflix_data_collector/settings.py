f3a5a1addbc50267783fce920a02bb860597085a
Kh. Ashiqur Rahman <ashiqur.buet16@gmail.com>
2021-04-21 05:27:50 +0600

    refactor

--- bookflix-data-collector/src/bookflix_data_collector/settings.py
+++ bookflix-data-collector/src/bookflix_data_collector/settings.py
@@ -73,12 +73,24 @@ WSGI_APPLICATION = 'bookflix_data_collector.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if DEBUG and not os.environ.get("AWS_RDS_POSTGRES_HOST_URL"):
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.environ.get('AWS_RDS_POSTGRES_DB_NAME'),
+            'USER': os.environ.get('AWS_RDS_POSTGRES_MASTER_USERNAME'),
+            'PASSWORD': os.environ.get('AWS_RDS_POSTGRES_MASTER_PASSWORD'),
+            'HOST': os.environ.get('AWS_RDS_POSTGRES_HOST_URL'),
+            'PORT': 5432
+        }
+    }
 
 
 # Password validation
